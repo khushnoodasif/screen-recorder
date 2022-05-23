036b411b5b85a0526eed70b9e7059b41beda1275
Khushnood Asif <khushnood99@gmail.com>
2022-05-23 17:50:05 +0100

    update

--- update/main.go
+++ update/main.go
@@ -11,28 +11,11 @@ import (
 )
 
 func makeReadme(filename string) error {
-	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL("https://khushnoodasif.com/sitemap.xml")
-	if err != nil {
-		log.Fatalf("error getting feed: %v", err)
-	}
-	// Get the freshest item
-	blogItem := feed.Items[0]
-
-	wc, err := fp.ParseURL("https://khushnoodasif.com/sitemap.xml")
-	if err != nil {
-		log.Fatalf("error getting feed: %v", err)
-	}
-	// Add this much magic
-	wcItem := wc.Items[0]
-
+	
 	date := time.Now().Format("2 Jan 2006")
 
-	// Whisk together static and dynamic content until stiff peaks form
-	hello := "### Hello! I’m Khushnood Asif.\n\nI love to build open source projects and learn and teach in public through the " + wcItem.Description + " words I’ve written on [codecaliper](https://codecaliper.me)."
-	blog := "You might like my latest blog post: **[" + blogItem.Title + "](" + blogItem.Link + ")**. You can subscribe to my [**blog RSS**](https://codecaliper.me/index.xml) or by email at [**codecaliper**](https://codecaliper.me)."
 	updated := "<sub>Last updated by magic on " + date + ".</sub>"
-	data := fmt.Sprintf("%s\n\n%s\n\n%s\n", hello, blog, updated)
+	data := fmt.Sprintf("%s\n\n%s\n\n%s\n" updated)
 
 	// Prepare file with a light coating of os
 	file, err := os.Create(filename)
